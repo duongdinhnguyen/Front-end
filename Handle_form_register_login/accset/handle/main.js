c1b4ec3a1ac150c9b4506945e4a34f67f94227ab
duongdinhnguyen <dinhduongco1308@gmail.com>
2021-12-08 18:23:57 +0700

    update CSS of error, exactly when handle event oninput, onblur

--- Handle_form_register_login/accset/handle/main.js
+++ Handle_form_register_login/accset/handle/main.js
@@ -1,20 +1,100 @@
-var error_notification = "Chưa điền thông tin";
+var error_notification = "*Chưa điền thông tin";
 var list_input = document.querySelectorAll(".form-element-input");
-
-// function blurUserName(value){
-    
-// }
-list_input.forEach(input_item =>{
-    input_item.onblur = function(){
-        var value_input = this.value;
-        var error_Element = this.parentElement.querySelector(".form-element-errorNotification");
-
+var value_password ="";
+function checkInput(item, callback){
+    var error_Element = item.parentElement.querySelector(".form-element-errorNotification");
+    // xóa thông báo lỗi khi đang nhập dữ liệu
+    item.oninput = function(){
+        if(item.classList.contains("input-success", "input-error")){
+            item.classList.remove("input-success");
+            this.classList.remove("input-error");
+        }
+        
+        error_Element.innerText = ``;
+    }
+    // thông báo khi blur ra khỏi trường input
+    item.onblur = function(){
+        var value_input = this.value.trim();
+        
         if(!value_input){
             error_Element.innerText = error_notification;
+            this.classList.add("input-error");
+
         }
         else{
-            error_Element.innerText = ``;
+            // kiểm tra đúng định dang chưa
+            callback(this, error_Element, value_password);
         }
+    }
+}
+// Đúng định dạng text
+function textInput(item){
+    item.classList.add("input-success");
+}
+
+// đúng định dạng email
+function emailInput(item, error_Element){
+    var errorEmail = "*Sai định dạng email.";
+    var emailInnitial =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(emailInnitial.test(item.value)){
+        item.classList.add("input-success");
+        
+    }
+    else {
+        if(item.classList.contains("input-success")){
+            item.classList.remove("input-success");
+        }
+        error_Element.innerText = errorEmail;
+        item.classList.add("input-error");
+    }
+}
+
+
+// đúng đính dạng password
+function passwordInput(item, error_Element){
+    var errorPassword = "*6-20 ký tự gồm 1 kí tự hoa, 1 kí tự số, 1 kí tự đặc biệt.";
+    var passWordInitial  = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    // Password từ 6-20 ký tự chứa 1 chữ số, 1 chữ hoa, 1 chữ thường
+    if(item.value.match(passWordInitial)){
+        item.classList.add("input-success");
+        value_password = item.value;
+    }
+    else{
+        if(item.classList.contains("input-success")){
+            item.classList.remove("input-success");
+        }
+        error_Element.innerText = errorPassword;
+        item.classList.add("input-error");
+    }
+    
+}
+function passwordRetypeInput(item, error_Element, value_email){
+    var errorRetype = "*Không đúng password";
+    if(String(item.value) === String(value_email) ){
+        item.classList.add("input-success");
+    }
+    else{
+        if(item.classList.contains("input-success")){
+            item.classList.remove("input-success");
+        }
+        error_Element.innerText = errorRetype;
+        item.classList.add("input-error");
+    }
+}
+
+list_input.forEach(item =>{
+    
+    if(item.id == "user-name" ){
+        checkInput(item, textInput);
+    }
+    else if(item.id == "email"){
+        checkInput(item, emailInput);
+    }
+    else if(item.id == "password"){
+        checkInput(item, passwordInput);
+    }
+    else if(item.id == "retype-password"){
+        checkInput(item, passwordRetypeInput);
     }
     
 });
